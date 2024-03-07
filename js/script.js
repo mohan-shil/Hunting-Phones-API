33dfafca206fb814d5bd99f526cb571261e0adb0
Mohan Shil <121026741+mohan-shil@users.noreply.github.com>
2024-03-08 00:26:41 +0100

    added final components

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const loadPhone = async (searchText, isShowAll) => {
         showAllContainer.classList.add('hidden');
     }
 
-    console.log('is show all ', isShowAll)
+    // console.log('is show all ', isShowAll)
 
     // display first 12 phones if not show all
     
@@ -48,8 +48,8 @@ const loadPhone = async (searchText, isShowAll) => {
         <div class="card-body">
             <h2 class="card-title"> ${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+            <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary text-white font-bold text-lg">Show Details</button>
             </div>
         </div>
         `;
@@ -64,6 +64,31 @@ const loadPhone = async (searchText, isShowAll) => {
     // hide loading spinner 
         toggleLoadingSpinner(false);
 }
+    // load single phone details 
+    const handleShowDetail = async (id) => {
+        // console.log('clicked', id);
+
+
+        const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+        const data = await res.json();
+        const phone = data.data;
+
+        showPhoneDetails(phone);
+    }
+
+    const showPhoneDetails = (phone) => {
+        // console.log(phone);
+
+        const phoneName = document.getElementById('phone-name');
+        phoneName.innerText = phone.name;
+
+        const showDetails = document.getElementById('show-detail-container');
+        showDetails.innerHTML = ` <img src="${phone.image}" alt="" /> `
+
+
+        showDetailModal.showModal();
+    }
+    
 
     //handle search button 
     const handleSearch = (isShowAll) => {
